eea301e3b5c8d01fb6f6237916e401d708568555
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-01-21 20:05:48 -0500

    Set DrSEUs to use 460800 as the baudrate.

--- test_runs.sh
+++ test_runs.sh
@@ -1,6 +1,7 @@
 # Makes a campaign
-./drseus.py --serial /dev/ttyUSB1 --prompt safeword --jtag_ip raspberrypi.local --timeout 40000 new --arch a9 -c unused -t 1 -o output.txt
+#./drseus.py --serial /dev/ttyUSB1 --prompt safeword --jtag_ip raspberrypi.local --timeout 40000 new --arch a9 -c unused -t 1 -o output.txt
 
+./drseus.py --serial /dev/ttyUSB1 --baud 460800 --prompt safeword --jtag_ip raspberrypi.local --timeout 40000 new --arch a9 -c unused -t 1 -o output.txt
 
 # Runs a campaign with n injection iteration using p processes
 #./drseus.py --timeout 5 --serial /dev/ttyUSB1 --prompt safeword inject -n 25 -i 1 -p 1 -T 0 -l 0
