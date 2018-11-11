2786512d4aa54474a23c32ab169776b08ddb00e5
James Marshall <jcmarsh@gwmail.gwu.edu>
2018-11-11 17:02:46 -0500

    Up the timeout.

--- test_runs.sh
+++ test_runs.sh
@@ -1,7 +1,7 @@
 # Makes a campaign
 #./drseus.py --serial /dev/ttyUSBzybo --prompt safeword --jtag_ip rpi3open.local --timeout 40000 new --arch a9 -c unused -t 1 -o output.txt
 
-./drseus.py --serial /dev/ttyUSBzybo --baud 460800 --prompt safeword --jtag_ip localhost --timeout 40000 new --arch a9 -c unused -t 1 -o output.txt
+./drseus.py --serial /dev/ttyUSBzybo --baud 460800 --prompt safeword --jtag_ip localhost --timeout 80000 new --arch a9 -c unused -t 1 -o output.txt
 
 # Runs a campaign with n injection iteration using p processes
 #./drseus.py --timeout 5 --serial /dev/ttyUSBzybo --prompt safeword inject -n 25 -i 1 -p 1 -T 0 -l 0
